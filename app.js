bb789a47bb2838a9c3ad4035460fdbd7e3859959
RAGUNATH-R-30 <62740632+RAGUNATH-R-30@users.noreply.github.com>
2024-07-25 17:37:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://rrestate.netlify.app/",
     credentials: true,
   })
 );
